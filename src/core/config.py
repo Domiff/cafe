4b11aa55d6c8e94e6f4d7a5a9380270aa011efbe
dmitriy levykin <dmlegasy@gmail.com>
2026-08-15 20:06:46 +0300

    feat: add config for s3

--- src/core/config.py
+++ src/core/config.py
@@ -49,11 +49,21 @@ class AdminSettings(AppSettings):
     SECRET_KEY: str = "admin"
 
 
+class StorageSettings(AppSettings):
+    AWS_ACCESS_KEY_ID: str = ""
+    AWS_SECRET_ACCESS_KEY: str = ""
+    AWS_S3_BUCKET_NAME: str = ""
+    AWS_S3_ENDPOINT_URL: str = ""
+    AWS_DEFAULT_ACL: str = ""
+    AWS_S3_USE_SSL: bool = True
+
+
 class Settings(AppSettings):
     app: AppSettings = AppSettings()
     db: DBSettings = DBSettings()
     admin: AdminSettings = AdminSettings()
     logging: LoginSettings = LoginSettings()
+    storage: StorageSettings = StorageSettings()
 
 
 settings = Settings()
